d4a9e95ace18de5508175fbdaa18e0daa72e7781
QSpiders <QSpiders@DESKTOP-U3SNJL3>
2024-04-04 15:44:19 +0530

    file upload by auto it

---
+++ Automation/src/fileupload/HandlingFileUploadPoupByusingRobot2.java
@@ -0,0 +1,25 @@
+package fileupload;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HandlingFileUploadPoupByusingRobot2 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://demoapps.qspiders.com/ui/fileUpload?sublist=0");
+		Thread.sleep(3000);
+		driver.findElement(By.name("file")).sendKeys("C:\\Users\\QSpiders\\Downloads\\order_1640960.pdf");
+
+	}
+
+}
