5ed568e589a40c231b24f0e414bfb0facdae048e
QSpiders <QSpiders@DESKTOP-U3SNJL3>
2024-04-08 15:52:27 +0530

    data driven framework

---
+++ Automation/src/DataDrivenFramework/FetchingDataFromPropertyFile.java
@@ -0,0 +1,24 @@
+package DataDrivenFramework;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class FetchingDataFromPropertyFile {
+	
+	
+	public static void main(String[] args) throws IOException {
+		
+		FileInputStream fis = new FileInputStream("./testData/CommanData.properties");
+		Properties prop = new Properties();
+		prop.load(fis);
+		String url = prop.getProperty("Url");
+		System.out.println(url);
+		String username = prop.getProperty("username");
+		System.out.println(username);
+		
+		
+		
+	}
+
+}
